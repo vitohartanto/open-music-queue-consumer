16531a2ec3f91e13a2010c01efa0f6693402b0a7
Vito <vitohartanto@mail.ugm.ac.id>
2024-10-09 13:42:17 +0700

    selesai

--- src/Consumer.js
+++ src/Consumer.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const amqp = require('amqplib');
 const PlaylistsService = require('./PlaylistsService');
 const MailSender = require('./MailSender');
-const Listener = require('./Listener');
+const Listener = require('./listener');
 
 const init = async () => {
   const playlistsService = new PlaylistsService();
@@ -13,11 +13,11 @@ const init = async () => {
   const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
   const channel = await connection.createChannel();
 
-  await channel.assertQueue('export:playlist', {
+  await channel.assertQueue('export:playlists', {
     durable: true,
   });
 
-  channel.consume('export:playlist', listener.listen, { noAck: true });
+  channel.consume('export:playlists', listener.listen, { noAck: true });
 };
 
 init();
